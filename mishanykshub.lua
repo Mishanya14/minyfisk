6b231c7d22355b94018484f1e41fd01c40882dcd
Mishanya14 <132597169+Mishanya14@users.noreply.github.com>
2023-05-04 22:13:08 +0300

    Update mishanykshub.lua

--- mishanykshub.lua
+++ mishanykshub.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("GamesHub - by mishanya)!#4518", "RJTheme4")
+local Window = Library.CreateLib("minyfisk - by mishanya)!#4518", "RJTheme4")
 
 local Tab = Window:NewTab("Main")
 
